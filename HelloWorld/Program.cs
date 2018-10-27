e6db0c8745837c59625e8e1556e4e187d0ec97cb
Abhishek Behera <abehera1992@gmail.com>
2018-10-27 13:48:20 -0400

    first commit for MS TestAutomation.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            string name = "Hello Abhishek";
+            return name;
+        }
+    }
+}
